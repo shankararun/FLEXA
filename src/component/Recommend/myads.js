b54fe42d75726b543f317058ee4d0154870aff17
yukesh <68677236+yukeshyuks@users.noreply.github.com>
2022-01-28 21:42:03 +0530

    Update myads.js

--- src/component/Recommend/myads.js
+++ src/component/Recommend/myads.js
@@ -6,10 +6,13 @@ import Footer from '../Footer/Footer'
 const Myads = () => {
     return (
         <><div>
+      {/* Navbar component is imported from the navbar.js present in "src/Component/Navbar/Navbar.js" */}
             <Navbar />
         </div><div>
+     {/* Navbar component is imported from the navbar.js present in "src/Components/Recommend/Recommend.js" */}
                 <Recommend />
             </div><div>
+      {/* Navbar component is imported from the navbar.js present in "src/Component/Footer/Footer.js" */}
                 <Footer />
             </div></>
     )
