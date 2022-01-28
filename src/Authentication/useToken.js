0db2225bab6370aff508f764990b05c8672c142c
yukesh <68677236+yukeshyuks@users.noreply.github.com>
2022-01-28 21:00:53 +0530

    Update useToken.js

--- src/Authentication/useToken.js
+++ src/Authentication/useToken.js
@@ -1,5 +1,9 @@
+
+/*useState hook is used which is imported from react*/
 import { useState } from 'react';
 
+
+/* useToken function returns the authentication token of the user*/
 export default function useToken() {
   const getToken = () => {
     const tokenString = sessionStorage.getItem('token');
@@ -9,8 +13,7 @@ export default function useToken() {
 
   const [token, setToken] = useState(getToken());
 
-  
-
+/*saveToken saves the token generated in the sessionstorage*/
   const saveToken = userToken => {
     sessionStorage.setItem('token', JSON.stringify(userToken));
     setToken(userToken.token);
