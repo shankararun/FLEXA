d9b277c5efe816fb2ad421cfbd02b38247aa3b35
shankararun <73650548+shankararun@users.noreply.github.com>
2022-01-29 11:57:29 +0530

    Update Categoryscreen.js

--- src/Category/Categoryscreen.js
+++ src/Category/Categoryscreen.js
@@ -7,9 +7,11 @@ import Footer from "../component/Footer/Footer";
 
 const Categoryscreen = () => {
     return (
+        {/* Navbar component is imported from the navbar.js present in "src/Components/Navbar.js" */}
         <><div><Navbar /></div>
+        {/* Category component is imported from the Category.js present in "src/Category/Category.js" */}
         <div><Category /></div>
-        {/* Footer component is imported from the Footer.js present in "src/Component/Footer/Footer.js" */}
+        {/* Footer component is imported from the Footer.js present in "src/component/Footer/Footer.js" */}
         <div><Footer /></div></>
     )
 }
